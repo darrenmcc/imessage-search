6834a2662219b888d3ce199d0783359885d4d1f1
Darren McCleary <darren.rmc@gmail.com>
2016-12-09 14:11:48 -0500

    Some cleanup

--- main.go
+++ main.go
@@ -15,13 +15,18 @@ var (
 	text  = flag.String("q", "", "the text to search for")
 
 	fromMap = map[int]string{
-		0: "You:\t",
-		1: "Them:\t",
+		0: "Them",
+		1: "You",
 	}
 )
 
-func main() {
+type line struct {
+	IsMe    int
+	Date    string
+	Message string
+}
 
+func main() {
 	flag.Parse()
 
 	usr, err := user.Current()
@@ -36,18 +41,23 @@ func main() {
 	defer db.Close()
 
 	query := fmt.Sprintf(`
-        SELECT  is_from_me, text 
+        SELECT  
+            is_from_me, 
+            datetime(date + strftime('%%s', '2001-01-01'), 'unixepoch', 'localtime'),
+            trim(text) 
         FROM    message 
-        WHERE   text LIKE '%%%s%%' --case-insensitive
-        AND     handle_id=(
-            SELECT  handle_id
-            FROM    chat_handle_join 
-            WHERE   chat_id=(
-                SELECT  ROWID 
-                FROM    chat 
-                WHERE   guid='iMessage;-;%s'
+        WHERE   
+            text LIKE '%%%s%%' --case-insensitive
+            AND     handle_id=(
+                SELECT  handle_id
+                FROM    chat_handle_join 
+                WHERE   chat_id=(
+                    SELECT  ROWID 
+                    FROM    chat 
+                    WHERE   guid='iMessage;-;%s'
+                )
             )
-        )`, *text, *phone)
+            AND NOT text LIKE '%%Digital Touch Message%%'`, *text, *phone)
 
 	rows, err := db.Query(query)
 	if err != nil {
@@ -57,11 +67,13 @@ func main() {
 
 	for rows.Next() {
 		var isMe int
-		var msg string
-		err = rows.Scan(&isMe, &msg)
+		var msg, date string
+		err = rows.Scan(&isMe, &date, &msg)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(fromMap[isMe], msg)
+
+		fmt.Printf("%s...%s:\t%s\n", date, fromMap[isMe], msg)
 	}
+
 }
